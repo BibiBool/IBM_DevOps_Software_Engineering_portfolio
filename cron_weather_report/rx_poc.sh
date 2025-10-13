026cd3c3bd546c2aac852a176988c08174173fbd
Bibibool <dorinvilbruel@gmail.com>
2025-10-13 21:44:44 +0200

    Enhance weather report script with temperature extraction logic

--- cron_weather_report/rx_poc.sh
+++ cron_weather_report/rx_poc.sh
@@ -1,2 +1,9 @@
 city=casablanca
+
+# Request temperature data for the city
 curl -s wttr.in/$city?T --output weather_report 
+
+# To extract the current temperature
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.'| grep -Eo -e '-?[[:digit:]].*')
+echo "The current Temperature of $city: $obs_temp"
+
