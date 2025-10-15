08499450090b7eb39480cb9e9e7da9e83c87482f
Bibibool <dorinvilbruel@gmail.com>
2025-10-15 21:41:46 +0200

    Log temperature data with date in weather report script

--- cron_weather_report/rx_poc.sh
+++ cron_weather_report/rx_poc.sh
@@ -10,3 +10,12 @@ echo "The current Temperature of $city: $obs_temp"
 # To extract temperature at noon
 fc_temp=$(curl -s wttr.in/$city?T | head -n 23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
 echo "The forcasted temperature at noon of $city: $fc_temp" 
+
+#Store current day, month, and year in variables
+day=$(TZ='Morocco/Casablanca' date -u +%d)
+month=$(TZ='Morocco/Casablanca' date -u +%m)
+year=$(TZ='Morocco/Casablanca' date -u +%Y)
+
+# Merge the fields to add them as a record in the log file
+record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp C")
+echo $record>> rx_poc.log
