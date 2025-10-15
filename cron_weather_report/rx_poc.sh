87bf6346ac955b4148a8dd89090d695f32c706e2
Bibibool <dorinvilbruel@gmail.com>
2025-10-15 21:03:28 +0200

    Add .gitignore and enhance temperature extraction in weather report script

--- cron_weather_report/rx_poc.sh
+++ cron_weather_report/rx_poc.sh
@@ -7,3 +7,6 @@ curl -s wttr.in/$city?T --output weather_report
 obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.'| grep -Eo -e '-?[[:digit:]].*')
 echo "The current Temperature of $city: $obs_temp"
 
+# To extract temperature at noon
+fc_temp=$(curl -s wttr.in/$city?T | head -n 23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
+echo "The forcasted temperature at noon of $city: $fc_temp" 
