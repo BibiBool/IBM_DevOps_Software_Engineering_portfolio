8abd4c83a8b4376c2c205322a8310cf28805533e
Bibibool <dorinvilbruel@gmail.com>
2025-10-16 16:55:35 +0200

    Refactor weather report scripts and update accuracy logging; add synthetic historical data

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ This is where I gather all the worthy projects from the course
 
 ## Project 1: Cron weather report
 
-The goal is to write a bash job to request the temperature of a city everyday 
-at noon and log it in a file
+The goal is to write a bash job to request the temperature of a city everyday at noon and log it in a file
--- cron_weather_report/rx_poc.sh
+++ cron_weather_report/rx_poc.sh
@@ -1,21 +1,27 @@
-city=casablanca
+#!/bin/bash
 
-# Request temperature data for the city
-curl -s wttr.in/$city?T --output weather_report 
+#Assign city name as Casablanca
+city=Casablanca
 
-# To extract the current temperature
-obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.'| grep -Eo -e '-?[[:digit:]].*')
-echo "The current Temperature of $city: $obs_temp"
+#Obtain the weather report for Casablanca
+curl -s wttr.in/$city?T --output weather_report
 
-# To extract temperature at noon
-fc_temp=$(curl -s wttr.in/$city?T | head -n 23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*')
-echo "The forcasted temperature at noon of $city: $fc_temp" 
+#To extract Current Temperature
+obs_temp=$(curl -s wttr.in/$city?T | grep -m 1 '°.' | grep -Eo -e '-?[[:digit:]].*' | cut -d '(' -f1)
+echo "The current Temperature of $city: $obs_temp °C"
 
-#Store current day, month, and year in variables
+# To extract the forecast tempearature for noon tomorrow
+fc_temp=$(curl -s wttr.in/$city?T | head -23 | tail -1 | grep '°.' | cut -d 'C' -f2 | grep -Eo -e '-?[[:digit:]].*' | cut -d '(' -f1)
+echo "The forecasted temperature for noon tomorrow for $city : $fc_temp °C"
+
+#Assign Country and City to variable TZ
+TZ='Morocco/Casablanca'
+
+# Use command substitution to store the current day, month, and year in corresponding shell variables:
 day=$(TZ='Morocco/Casablanca' date -u +%d)
-month=$(TZ='Morocco/Casablanca' date -u +%m)
-year=$(TZ='Morocco/Casablanca' date -u +%Y)
+month=$(TZ='Morocco/Casablanca' date +%m)
+year=$(TZ='Morocco/Casablanca' date +%Y)
 
-# Merge the fields to add them as a record in the log file
-record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp C")
-echo $record>> rx_poc.log
+# Log the weather
+record=$(echo -e "$year\t$month\t$day\t$obs_temp\t$fc_temp")
+echo $record>>rx_poc.log
